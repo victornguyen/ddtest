133e286cf9a62b7c6decbac1168ce17b32bd63af
Victor Nguyen <vic.nguyen@gmail.com>
2015-09-14 22:17:44 +1000

    Add carousel

---
+++ source/javascripts/carousel.js
@@ -0,0 +1,15 @@
+//= require "jquery"
+//= require "slick-carousel"
+
+(function($){
+
+  $(function(){
+
+    $('.carousel').slick({
+      dots: true,
+      mobileFirst: true
+    });
+
+  });
+
+}(jQuery));
